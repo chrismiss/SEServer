ccb0c28742b3c132f57df075951025ba1ced33d0
CJ Mazur <mazucj22@wclive.westminster.edu>
2019-04-03 20:59:37 -0400

    Initial Commit
    CJ made first commit to include a set of fail/pass test cases.

---
+++ SEServer/src/AutomaticFailureTests.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AutomaticFailureTests {
+
+	@Test
+	void test() {
+		fail("This test is supposed to fail");
+	}
+	
+	@Test
+	void test1() {
+		assertTrue("This test is supposed to pass", true);
+	}
+
+}
