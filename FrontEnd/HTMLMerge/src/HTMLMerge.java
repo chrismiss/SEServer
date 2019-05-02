d4ffd63daf3c60b655ba66f8fd8e0569d72d8b20
Matthew <49175332+MattBollR313@users.noreply.github.com>
2019-05-01 21:35:15 -0400

    Update HTMLMerge.java

--- FrontEnd/HTMLMerge/src/HTMLMerge.java
+++ FrontEnd/HTMLMerge/src/HTMLMerge.java
@@ -5,16 +5,16 @@ import java.io.*;
 public class HTMLMerge {
 
 	public static void main(String[] args) {
-			ArrayList<String> list = new ArrayList<String>();
-			list.add("log-in.html");
-			list.add("calendar.html");
-			Scanner fin = null;
-			try {
-				fin = new Scanner(new FileInputStream(list.get(0)));
-			} catch (FileNotFoundException e) {
-				System.out.println("Incorrect file name");
-				System.exit(-1);
-			}
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("log-in.html");
+		list.add("calendar.html");
+		Scanner fin = null;
+		try {
+			fin = new Scanner(new FileInputStream(list.get(0)));
+		} catch (FileNotFoundException e) {
+			System.out.println("Incorrect file name");
+			System.exit(-1);
+		}
 	}
 
 }
