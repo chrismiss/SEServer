5c816381e5599506bd09ef010d95dd57dd6d299e
Matthew <49175332+MattBollR313@users.noreply.github.com>
2019-05-07 23:44:12 -0400

    Update HTMLMerge.java

--- FrontEnd/HTMLMerge/src/HTMLMerge.java
+++ FrontEnd/HTMLMerge/src/HTMLMerge.java
@@ -5,24 +5,48 @@ import java.io.*;
 public class HTMLMerge {
 
 	public static void main(String[] args) throws IOException {
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("log-in.html");
-		list.add("calendar.html");
+		ArrayList<String> listOfFiles = new ArrayList<String>();
+		listOfFiles.add("template.html");
+		listOfFiles.add("log-in.html");
+		listOfFiles.add("calendar.html");
+		listOfFiles.add("templateEnd.html");
+
+		ArrayList<String> fileList = writeTo(listOfFiles.get(0));
+		Writer w = new FileWriter("index1.html");
+		for (int i = 0; i < fileList.size(); i++) {
+			w.write(fileList.get(i) + "\n");
+		}
+		for (int index = 1; index < listOfFiles.size() - 1; index++) {
+			fileList = writeTo(listOfFiles.get(index));
+			for (int i = 0; i < fileList.size(); i++) {
+				w.write("\t" + fileList.get(i) + "\n");
+			}
+		}
+		fileList = writeTo(listOfFiles.get(listOfFiles.size()-1));
+		for (int i = 0; i < fileList.size(); i++) {
+			w.write(fileList.get(i) + "\n");
+		}
+		w.close();
+	}
+
+	/*
+	 * Takes a path string as an argument.
+	 * Scans file from path and puts contents into
+	 * an array list that's returned to user.
+	 */
+	private static ArrayList<String> writeTo(String path) {
 		Scanner fin = null;
 		try {
-			fin = new Scanner(new FileInputStream(list.get(0)));
+			fin = new Scanner(new FileInputStream(path));
 		} catch (FileNotFoundException e) {
 			System.out.println("Incorrect file name");
 			System.exit(-1);
 		}
-		Writer w = new FileWriter("index1.html");
-		// Begins write process into index.html starting with head
-		w.write("<!DOCTYPE html>");
-		w.write("<html>");
-
+		ArrayList<String> list = new ArrayList<String>();
 		while (fin.hasNextLine()) {
-
+			list.add(fin.nextLine());
 		}
+		fin.close();
+		return list;
 	}
-
 }
